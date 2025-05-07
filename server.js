dcc5aacff373e60292b3a9f697f4d27e002ba90a
Alan <Alan2003@sju.ac.kr>
2025-05-07 11:39:24 +0900

    as

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ const usersCache = {};
 // CORS configuration
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
-    methods: ["GET"], // Adjust methods if needed
-    allowedHeaders: ["x-tenant-id"],
+    origin: "https://dashbro.netlify.app", // Replace with your actual frontend URL
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["x-tenant-id"], // Adjust headers as needed
   })
 );
 
